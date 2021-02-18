9fd2f17b9c3ae13eb6a0727a6b46eaef80254a9c
LeoBourret <bourretleo@hotmail.fr>
2021-02-18 14:59:54 +0100

    patched segfault for no map, gg julien

--- srcs/error.c
+++ srcs/error.c
@@ -12,6 +12,25 @@
 
 #include "cub3d.h"
 
+char	**gen_error_list_re(char **error)
+{
+	error[9] = "Error: map not found in the .cube file\n";
+	error[10] = "Error: map is not set properly\n";
+	error[11] = "Error: map is not closed properly\n";
+	error[12] = "Error: multiple spawn point found in the map\n";
+	error[13] = "Error: spawn point not found in the map\n";
+	error[14] = "Error: unknown identifier is set\n";
+	error[15] = "Error: incorrect .cub map\n";
+	error[16] = "Error: unknown argument\n";
+	error[17] = "Error: map not given during execution\n";
+	error[18] = "Error: too many argument\n";
+	error[19] = "Error: cannot read file\n";
+	error[20] = "Error: cannot write in save file\n";
+	error[21] = "Error: cannot load xpm file\n";
+	error[22] = NULL;
+	return (error);
+}
+
 char	**gen_error_list(void)
 {
 	char **error;
@@ -32,30 +51,7 @@ char	**gen_error_list(void)
 	error[8] = "Error: ceilling color not set or incorrect. \
 	RGB must contain numbers, and one comma in between \
 	(spaces are allowed, but commas are mandatory).\n";
-	error[22] = NULL;
-	return (error);
-}
-
-char	**gen_error_list_re(void)
-{
-	char **error;
-
-	if (!(error = malloc(sizeof(char *) * 23)))
-		return (NULL);
-	error[9] = "Error: map not found in the .cube file\n";
-	error[10] = "Error: map is not set properly\n";
-	error[11] = "Error: map is not closed properly\n";
-	error[12] = "Error: multiple spawn point found in the map\n";
-	error[13] = "Error: spawn point not found in the map\n";
-	error[14] = "Error: unknown identifier is set\n";
-	error[15] = "Error: incorrect .cub map\n";
-	error[16] = "Error: unknown argument\n";
-	error[17] = "Error: map not given during execution\n";
-	error[18] = "Error: too many argument\n";
-	error[19] = "Error: cannot read file\n";
-	error[20] = "Error: cannot write in save file\n";
-	error[21] = "Error: cannot load xpm file\n";
-	return (error);
+	return (gen_error_list_re(error));
 }
 
 int		error_manager(int error, t_p *par)
